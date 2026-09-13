28d51a3635db7d89643b210e9fed845245ce227e
chen-li888 <chen.li@coffeechatnetwork.io>
2026-09-12 23:56:47 -0400

    wip...

---
+++ Source/LiAIChat/Questing/QuestNode_RequireColonyAge.cs
@@ -0,0 +1,28 @@
+﻿using RimWorld;
+using RimWorld.QuestGen;
+using Verse;
+
+namespace LiAIChat.Questing
+{
+    public class QuestNode_RequireColonyAge : QuestNode
+    {
+        public SlateRef<int> minDays;
+
+        protected override bool TestRunInt(Slate slate)
+        {
+            if (Find.TickManager == null)
+                return false;
+
+            int currentTick = Find.TickManager.TicksGame;
+            int requiredTicks = minDays.GetValue(slate) * GenDate.TicksPerDay;
+
+            return currentTick >= requiredTicks;
+        }
+
+        protected override void RunInt()
+        {
+            // Generation gate only.
+            // No runtime QuestPart is required.
+        }
+    }
+}
